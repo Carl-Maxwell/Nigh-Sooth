423e7a1ce14b2c2f8f757288b511dab2e9843843
= <carl.maxwell@gmail.com>
2022-04-29 20:42:03 +0100

    code cleanup
    removing commented out code, whitespace changes, moving code around, etc
    
    no actual code changes

--- sooth_engine/entry_point.cpp
+++ sooth_engine/entry_point.cpp
@@ -104,8 +104,6 @@ void initialize() {
   image_array[ (u32)grid_tile::flagged ]  = image("resources/flag.png");
 }
 
-
-
 }
 
 int main(void) {
@@ -123,14 +121,14 @@ int main(void) {
 
   lapse::seed_rng(platform::get_application_start_time());
 
+  using minesweeper::grid_height;
+  using minesweeper::grid_width;
+
   // window:
   // u32 WIDTH = 1280/4, HEIGHT = 720/4;
   // u32 WIDTH = 1280, HEIGHT = 720;
   // u32 WIDTH = 1920/4 - 20, HEIGHT = 1080/4 - 20;
   i32 WIDTH = 16*18+20, HEIGHT = 16*12+20;
-
-  using minesweeper::grid_height;
-  using minesweeper::grid_width;
 
   // 18 x 12
 
@@ -218,71 +216,3 @@ int main(void) {
 
   return 0;
 }
-
-void set_event_callbacks() {
-  // glfwSetKeyCallback(glfw_window,
-  //   [](GLFWwindow* glfw_window, int glfw_key, int scancode, int action, int mods) {
-  //     std::cout << "keyboard event!" << "\n";
-
-  //     if (glfw_key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
-  //       glfwSetWindowShouldClose(glfw_window, GLFW_TRUE);
-  //     }
-  // } );
-
-  // glfwSetMouseButtonCallback(glfw_window,
-  //   [](GLFWwindow* glfw_window, int button, int action, int mods) {
-  //     bool pressed = action == GLFW_PRESS;
-
-  //     switch(button) {
-  //       case GLFW_MOUSE_BUTTON_1: { mouse.buttons.left  = pressed; } break; // left mouse
-  //       case GLFW_MOUSE_BUTTON_2: { mouse.buttons.right = pressed; } break; // right mouse
-  //       default: return;
-  //     }
-
-  //     switch (action) {
-  //       case GLFW_PRESS: {
-  //         // mouse button released
-  //         ;
-  //         std::cout << "mouse button pressed! \n";
-  //       } break;
-  //       case GLFW_RELEASE: {
-  //         // mouse button released
-  //         std::cout << "mouse button released! \n";
-  //       } break;
-  //     }
-  // } );
-
-  // glfwSetCursorPosCallback(glfw_window, 
-  //   [](GLFWwindow* glfw_window, f64 x_pos, f64 y_pos) {
-  //     mouse.x = x_pos;
-  //     mouse.y = y_pos;
-  // } );
-  
-  // //
-  // // Window events
-  // //
-
-  // glfwSetWindowPosCallback (glfw_window, 
-  //   [](GLFWwindow* glfw_window, i32 x_pos, i32 y_pos){
-  //     // std::cout << "Window position event x: " << x_pos << " y:" << y_pos << "\n";
-  // } );
-
-  // glfwSetWindowSizeCallback( glfw_window,
-  //   [](GLFWwindow* glfw_window, i32 width, i32 height){
-  //     std::cout << "window size event! ... this shouldn't realy happen" << "\n";
-  //     // TODO maybe throw an error?
-  // } );
-
-  // glfwSetWindowFocusCallback (glfw_window,
-  //   [](GLFWwindow* glfw_window, i32 focused){
-  //     std::cout
-  //       << "Event: window focus callback ... focused: " 
-  //       << (focused ? "focus gained!" : "focus lost : (") 
-  //       << "\n";
-  // } );
-
-  // glfwSetWindowRefreshCallback(glfw_window, [](GLFWwindow* glfw_window){
-  //   std::cout << "Event: window refresh event triggered\n";
-  // });
-
-};
